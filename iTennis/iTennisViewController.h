0b9d992997e7467aa154e1596f610afc86491e1f
Yathindra Naik <ydev@cs.utah.edu>
2013-01-23 20:59:52 -0700

    Parts1 and Parts2 are complete.

--- iTennis/iTennisViewController.h
+++ iTennis/iTennisViewController.h
@@ -13,22 +13,26 @@
     IBOutlet UIImageView *ball;
     IBOutlet UIImageView *racquet_yellow;
     IBOutlet UIImageView *racquet_green;
-    IBOutlet UIImageView *tapToBegin;
+    IBOutlet UILabel *tapToBegin;
     IBOutlet UILabel *computer_score;
     IBOutlet UILabel *player_score;
     
     CGPoint ballVelocity;
     NSInteger gameState;
+    
+    NSInteger player_score_value;
+    NSInteger computer_score_value;
 }
 
 @property (nonatomic, retain) IBOutlet UIImageView *ball;
-@property (nonatomic, retain) IBOutlet UIImageView *racquet_yellow;
-@property (nonatomic, retain) IBOutlet UIImageView *racquet_green;
-@property (nonatomic, retain) IBOutlet UIImageView *tapToBegin;
+@property (retain, nonatomic) IBOutlet UIImageView *racquet_yellow;
+@property (retain, nonatomic) IBOutlet UIImageView *racquet_green;
+@property (nonatomic, retain) IBOutlet UILabel *tapToBegin;
 @property (retain, nonatomic) IBOutlet UILabel *computer_score;
 @property (retain, nonatomic) IBOutlet UILabel *player_score;
 @property (nonatomic) CGPoint ballVelocity;
 @property (nonatomic) NSInteger gameState;
 
+- (void) reset:(BOOL) newGame;
 
 @end
--- iTennis/iTennisViewController.m
+++ iTennis/iTennisViewController.m
@@ -14,6 +14,9 @@
 #define kBallSpeedX         10
 #define kBallSpeedY         15
 
+#define kComputerSpeed      15
+
+#define kScoreToWin         5
 
 @interface iTennisViewController ()
 
@@ -37,7 +40,7 @@
 
 - (void) gameLoop
 {
-    //if (gameState == kGameStateRunning)
+    if (gameState == kGameStateRunning)
     {
         ball.center = CGPointMake(ball.center.x + ballVelocity.x, ball.center.y + ballVelocity.y);
         
@@ -45,10 +48,98 @@
             ballVelocity.x = -ballVelocity.x;
         }
         if (ball.center.y > self.view.bounds.size.height || ball.center.y < 0) {
-            ballVelocity.y = -ballVelocity.y;
+                     ballVelocity.y = -ballVelocity.y;
+        }
+        
+        // Collision with paddles
+        if (CGRectIntersectsRect(ball.frame, racquet_yellow.frame))
+        {
+            if (ball.center.y > racquet_yellow.center.y) {
+                ballVelocity.y = -ballVelocity.y;
+            }
+        }
+        if (CGRectIntersectsRect(ball.frame, racquet_green.frame))
+        {
+            if (ball.center.y < racquet_green.center.y) {
+                ballVelocity.y = -ballVelocity.y;
+            }
+        }
+        
+        // Begin Simple AI
+        if (ball.center.y < self.view.center.y)
+        {
+            if (ball.center.x < racquet_yellow.center.x) {
+                CGPoint compLocation = CGPointMake(racquet_yellow.center.x - kComputerSpeed, racquet_yellow.center.y);                               
+                racquet_yellow.center = compLocation;
+            }
+            if (ball.center.x > racquet_yellow.center.x) {
+                CGPoint compLocation = CGPointMake(racquet_yellow.center.x + kComputerSpeed, racquet_yellow.center.y);
+                racquet_yellow.center = compLocation;
+            }
+        }
+        
+        // Begin Scoring Game Logic
+        if (ball.center.y <= 0) {
+            player_score_value++;
+            [self reset:(player_score_value >= kScoreToWin)];
+        }
+        if (ball.center.y > self.view.bounds.size.height) {
+            computer_score_value++;
+            [self reset:(computer_score_value >= kScoreToWin)];
+        }
+    }
+    else {
+        if (tapToBegin.hidden == YES) {
+            tapToBegin.text = NO;
         }
     }
 }
+
+- (void) reset:(BOOL) newGame
+{
+    self.gameState = kGameStatePaused;
+    ball.center = self.view.center;
+    if (newGame)
+    {
+        if (computer_score_value > player_score_value) {
+            tapToBegin.text = @"Computer Wins!";
+        }
+        else {
+            tapToBegin.text = @"Player Wins!";
+        }
+        
+        player_score_value = 0;
+        computer_score_value = 0;
+    }
+    else {
+        tapToBegin.text = @"Tap To Begin";
+    }
+    
+    player_score.text = [NSString stringWithFormat:@"%d", player_score_value];
+    computer_score.text = [NSString stringWithFormat:@"%d", computer_score_value];
+}
+
+- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    if (gameState == kGameStatePaused)
+    {
+        gameState = kGameStateRunning;
+        tapToBegin.hidden = YES;
+    }
+    else if (gameState == kGameStateRunning)
+    {
+        [self touchesMoved:touches withEvent:event];
+    }
+}
+
+- (void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [[event allTouches] anyObject];
+    CGPoint location = [touch locationInView:touch.view];
+    CGPoint xlocation = CGPointMake(location.x, racquet_green.center.y);
+    racquet_green.center = xlocation;
+}
+
 - (void)viewDidUnload
 {
     [player_score release];
@@ -57,6 +148,17 @@
     computer_score = nil;
     [self setPlayer_score:nil];
     [self setComputer_score:nil];
+    [tapToBegin release];
+    tapToBegin = nil;
+    [self setRacquet_green:nil];
+    [self setRacquet_yellow:nil];
+    [self setRacquet_yellow:nil];
+    [self setRacquet_yellow:nil];
+    [self setRacquet_green:nil];
+    [computer_score release];
+    computer_score = nil;
+    [player_score release];
+    player_score = nil;
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -73,6 +175,14 @@
     [racquet_green release];
     [tapToBegin release];
     [ball release];
+    [tapToBegin release];
+    [racquet_green release];
+    [racquet_yellow release];
+    [racquet_yellow release];
+    [racquet_yellow release];
+    [racquet_green release];
+    [computer_score release];
+    [player_score release];
     [super dealloc];
 }
 @end
